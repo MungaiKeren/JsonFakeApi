42e8dd5a2498f4e2f76ccfb8f8c331bbfae71c4e
MungaiKeren <wambukeren@gmail.com>
2021-10-26 15:28:28 +0300

    code that sends a request to the json api

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+def home(request):
+    # get the list of todos
+    response = requests.get('https://jsonplaceholder.typicode.com/todos')
+
+    todos = response.json()
+    return render(request, "main_app/home.html", {"todos": todos})
