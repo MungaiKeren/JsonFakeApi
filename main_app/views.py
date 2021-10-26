16d890de7323fbcc53454c554c2955e62ceb6ee7
MungaiKeren <wambukeren@gmail.com>
2021-10-26 16:23:30 +0300

    get a single to do by is

--- main_app/views.py
+++ main_app/views.py
@@ -8,3 +8,16 @@ def home(request):
 
     todos = response.json()
     return render(request, "main_app/home.html", {"todos": todos})
+
+# get to do by id
+def todo_id(request):
+    id = {}
+    if 'id' in request.GET:
+        id = request.GET['id']
+        url = 'https://jsonplaceholder.typicode.com/todos'%id
+        response = request.get(url)
+
+        todo = response.json()
+    return render(request, 'main_app/single_todo.html', {"todo":todo})
+
+    
